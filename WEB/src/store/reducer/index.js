0fc35ddc64462d7bdd0761ea4a0190d7214dff44
sandrosd <sdezerio@gmail.com>
2021-07-04 18:55:15 -0300

    app inicial.

---
+++ WEB/src/store/reducer/index.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+
+import { ProcessReducer } from "./process";
+
+const rootReducers = combineReducers({
+  ProcessReducer,
+});
+
+export default rootReducers;
